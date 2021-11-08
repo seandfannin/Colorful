22734564a474a2272d1f021cf2ea26783bce7c98
Jade <87514374+hudginsjade02@users.noreply.github.com>
2021-11-07 22:07:18 -0500

    Update Colorful Code.py

--- Colorful Code.py
+++ Colorful Code.py
@@ -485,3 +485,17 @@ def challenge():
 
     #Printing random challenge
     #print(compChall)
+
+    # Login function; finished-ish
+def login():
+    
+    # This is the actual passcode login function
+    #    For good measure, I decided to ask for the username
+    #    as well, so that way the file reader can check
+    #    for matching information.
+    
+    inputFileName = "out.txt"
+    inputFile = open(inputFileName, "r", encoding="utf-8")
+    
+    userName = input("Username: ")
+    passcode = int(input("Passcode: "))
