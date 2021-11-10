4c2963f639a21355bf79d784cc50bbdc81ed3859
hannah <89534641+hannahh-lawsonn@users.noreply.github.com>
2021-11-10 15:21:21 -0500

    Update zipCode.py
    fixed gui to display therapist in box

--- zipCode.py
+++ zipCode.py
@@ -24,25 +24,29 @@ def zipCode():
 #only a few places, can add more later
     
     if city== "Daniels" or city== "Beckley":
-        print("Laurel Mountain Psychological, Inc  \n (304)-894-8930")
+        message=("Laurel Mountain Psychological, Inc  \n (304)-894-8930")
     elif city== "Princeton" or city=="Athens":
-        print("The Gibson Counseling Center \n (304)-487-9996")
+        message=("The Gibson Counseling Center \n (304)-487-9996")
     elif city== "Martinsburg" or city=="Shepherdstown":
-        print("Psychological Consulting Inc \n (304)-263-9095")        
+        message=("Psychological Consulting Inc \n (304)-263-9095")        
     elif city== "Bluefield":
-        print("Brain Matters Therapy \n (276)-970-9747")
+        message=("Brain Matters Therapy \n (276)-970-9747")
     elif city== "Wheeling" or city=="Weirton":
-        print("Muir Robin \n (304)-233-1200")        
+        message=("Muir Robin \n (304)-233-1200")        
     elif city== "Charleston" or city== "Saint Albans":
-        print("KPCC Counseling \n (304)-346-9689")
+        message=("KPCC Counseling \n (304)-346-9689")
     elif city== "Morgantown":
-        print("Fremouw-Sigley Psychological \n (304)-598-2300")        
+        message=("Fremouw-Sigley Psychological \n (304)-598-2300")        
     elif city== "Tazewell" or city== "Cedar Bluff":
-        print("Carilion Clinic \n (276)-988-8765")
+        message=("Carilion Clinic \n (276)-988-8765")
     elif city== "Bastian" or city== "Pearisburg":
-        print("Bland County Medical Clinic \n (276)-688-4331")
+        message=("Bland County Medical Clinic \n (276)-688-4331")
     else: 
-        print("Try another close city")
-
+        message=("Try another close city")
+    
+    title = "City: ", city
+    output = msgbox(message, title)
+        
+        
     
 zipCode()
